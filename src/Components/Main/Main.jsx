d13987f31bee8f4fe5254afeb1dce6dde8685a1a
tahanamdar30 <tahanamdar30@gmail.com>
2022-08-24 16:45:28 +0430

    add to favorite action done

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -5,6 +5,7 @@ const Main = () => {
   const [filterData, setFilterData] = useState([]);
   const [newFilter, setNewFilter] = useState([]);
   const [data, setData] = useState(null);
+  const [favorite, setFavorite] = useState([]);
 
   const baseUrl = "https://api.coinranking.com/v2/coins";
   const apiKey = "coinranking369971eebd30ea4d94a91d301bd9fb9099e6792808fd718c";
@@ -44,6 +45,10 @@ const Main = () => {
     }
   }
 
+  const addToFavorite = (item) => {
+    setFavorite((prevItem) => [...prevItem, item]);
+  };
+
   return (
     <div id="box-wrapper">
       <div className="box-title">
@@ -70,6 +75,7 @@ const Main = () => {
             <th>Name</th>
             <th>Price</th>
             <th>Change</th>
+            <th></th>
           </tr>
         )}
 
@@ -84,6 +90,14 @@ const Main = () => {
                 <td>{item.name}</td>
                 <td>${Number(item.price).toFixed(3)}</td>
                 <td>{item.change}%</td>
+                <td>
+                  <button
+                    className="favBtn"
+                    onClick={() => addToFavorite(item)}
+                  >
+                    add to favorite
+                  </button>
+                </td>
               </tr>
             );
           })}
