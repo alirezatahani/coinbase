9cecaf94fc0d83f28ea11dfa233aa9fd2eb53e6b
Behzad <saeedibehzad@gmail.com>
2022-09-02 12:20:30 +0430

    handle header

--- src/utils/apiClient.js
+++ src/utils/apiClient.js
@@ -1,13 +1,19 @@
-import axios from "axios"
+import axios from "axios";
 
-const makingUrl = (url)=>{
-    return process.env.REACT_APP_BASE_URL + url
-}
+const makingUrl = (url) => {
+  return process.env.REACT_APP_BASE_URL + url;
+};
 
- const apiClient =(options)=>{
-    const url = makingUrl(options.url)
+const apiClient = (options) => {
+  const url = makingUrl(options.url);
 
-    return axios({url,method:options.method})
-} 
+  return axios({
+    url,
+    method: options.method,
+    headers: {
+      "x-access-token": process.env.REACT_APP_API_KEY,
+    },
+  });
+};
 
-export default apiClient
+export default apiClient;
