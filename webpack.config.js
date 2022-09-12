59db8295ac7d7f3030306d85b387436ecb88ab0a
Behzad <saeedibehzad@gmail.com>
2022-09-12 11:28:45 +0430

    add plugin in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,88 +2,94 @@ const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 const webpack = require("webpack");
+const dotenv = require("dotenv");
 const CopyPlugin = require("copy-webpack-plugin");
 
-module.exports = {
-	mode: "development",
-	entry: {
-		app: "./src/index.tsx",
-	},
-	output: {
-		path: path.resolve(__dirname, "dist"),
-		publicPath: "/",
-		filename: "bundle.js",
-	},
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				use: ["babel-loader"],
-				exclude: /node_modules/,
-			},
-			{
-				test: /\.(ts|tsx)?$/,
-				use: "ts-loader",
-				exclude: /node_modules/,
-			},
-			{
-				test: /\.css$/,
-				use: ["style-loader", "css-loader"],
-				exclude: /\.module\.css$/,
-			},
-			{
-				test: /\.css$/,
-				use: [
-					"style-loader",
-					{
-						loader: "css-loader",
-						options: {
-							importLoaders: 1,
-							modules: true,
-						},
-					},
-				],
-				include: /\.module\.css$/,
-			},
-		],
-	},
-	resolve: {
-		plugins: [new TsconfigPathsPlugin({})],
-		extensions: [".tsx", ".ts", ".js"],
-	},
-	devServer: {
-		open: true,
-		hot: false,
-		port: 8080,
-		compress: true,
-		historyApiFallback: true,
-		devMiddleware: {
-			index: true,
-			writeToDisk: true,
-		},
-		static: {
-			directory: path.join(__dirname, "dist"),
-			publicPath: "/",
-		},
-	},
-	devtool: "cheap-module-source-map",
+dotenv.config();
 
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: path.resolve("./public/index.html"),
-		}),
-		new webpack.HotModuleReplacementPlugin(),
-		new CopyPlugin({
-			patterns: [
-				{
-					from: path.resolve("./public/manifest.json"),
-					to: path.resolve(__dirname, "dist"),
-				},
-				{
-					from: path.resolve("./public/logo1.png"),
-					to: path.resolve(__dirname, "dist"),
-				},
-			],
-		}),
-	],
+module.exports = {
+  mode: "development",
+  entry: {
+    app: "./src/index.tsx",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: ["babel-loader"],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(ts|tsx)?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+        exclude: /\.module\.css$/,
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
+              modules: true,
+            },
+          },
+        ],
+        include: /\.module\.css$/,
+      },
+    ],
+  },
+  resolve: {
+    plugins: [new TsconfigPathsPlugin({})],
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  devServer: {
+    open: true,
+    hot: false,
+    port: 8080,
+    compress: true,
+    historyApiFallback: true,
+    devMiddleware: {
+      index: true,
+      writeToDisk: true,
+    },
+    static: {
+      directory: path.join(__dirname, "dist"),
+      publicPath: "/",
+    },
+  },
+  devtool: "cheap-module-source-map",
+
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env": JSON.stringify(process.env),
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve("./public/index.html"),
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve("./public/manifest.json"),
+          to: path.resolve(__dirname, "dist"),
+        },
+        {
+          from: path.resolve("./public/logo1.png"),
+          to: path.resolve(__dirname, "dist"),
+        },
+      ],
+    }),
+  ],
 };
