456ce1c363ec6265f7335a487fc7f0b1a06add86
Behzad <saeedibehzad@gmail.com>
2022-09-13 10:02:31 +0430

    handle Route

---
+++ src/utils/router/Route.tsx
@@ -0,0 +1,5 @@
+import { RouteType } from "./router_types";
+
+export const Route = ({ component }: RouteType) => {
+  return component;
+};
---
+++ src/utils/router/router_types.d.ts
@@ -0,0 +1,6 @@
+import React, { ReactElement } from "react"
+
+export type RouteType = {
+    to:string,
+    component:ReactElement
+}
