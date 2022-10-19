199a843f25ae25cc67f6072e4d4f8fba49c8c63d
behzad <90694618+behzadiv@users.noreply.github.com>
2022-10-19 10:29:14 +0330

    handle searching coins by debounce

--- src/pages/home/content/Home.tsx
+++ src/pages/home/content/Home.tsx
@@ -1,22 +1,42 @@
-import { Tabs } from "antd";
-import { Badge, FlexWrapperBadge, HomeStyle } from "../style/home_styles";
+import React, { useCallback, useEffect, useState } from "react";
 import { useSelector } from "react-redux";
-import React from "react";
+import { debounce } from "lodash";
+import { Tabs } from "antd";
+import "antd/dist/antd.css";
+import { CoinsTable } from "@components/CoinsTable";
+import useFetch from "../../../hooks/useFetch";
+import { Badge, FlexWrapperBadge, HomeStyle } from "../style/home_styles";
 import AllCoins from "@modules/allCoins/content/allCoins";
 import NewCoins from "@modules/newCoins/content/newCoins";
 import FavoriteCoins from "@modules/favoriteCoins/content/favoriteCoins";
 import GainerCoins from "@modules/gainerCoins/content/gainerCoins";
 import LoserCoins from "@modules/loserCoins/content/loserCoins";
-import "antd/dist/antd.css";
 
 export default function Home() {
+  const [searchValue, setSearchValue] = useState("");
   const { TabPane } = Tabs;
+  const [{ loading, data }, doFetch] = useFetch();
 
   const favoriteReducers = useSelector((state: any) => state.FavoriteReducer);
   const { favoriteList } = favoriteReducers;
 
+  const searchCoin = async (searchText: string) => {
+    try {
+      await doFetch({
+        url: `/search-suggestions?query=${searchText}`,
+        method: "GET",
+      });
+    } catch (e) {}
+  };
+  const handler = useCallback(debounce(searchCoin, 600), []);
+  const onSearch = (searchText: string) => {
+    setSearchValue(searchText);
+    handler(searchText);
+  };
+
   return (
     <HomeStyle>
+      <input onChange={(e) => onSearch(e.target.value)} />(
       <Tabs defaultActiveKey="1">
         <TabPane tab="All" key="1">
           <AllCoins />
@@ -42,6 +62,7 @@ export default function Home() {
           <LoserCoins />
         </TabPane>
       </Tabs>
+      )
     </HomeStyle>
   );
 }
