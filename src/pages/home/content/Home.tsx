2064e635d37047471b520231b2cf526cd55aab37
behzad <90694618+behzadiv@users.noreply.github.com>
2022-10-20 19:26:43 +0330

    fix searchInput

--- src/pages/home/content/Home.tsx
+++ src/pages/home/content/Home.tsx
@@ -1,11 +1,16 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback,useState } from "react";
 import { useSelector } from "react-redux";
 import { debounce } from "lodash";
 import { Tabs } from "antd";
 import "antd/dist/antd.css";
 import { CoinsTable } from "@components/CoinsTable";
 import useFetch from "../../../hooks/useFetch";
-import { Badge, FlexWrapperBadge, HomeStyle } from "../style/home_styles";
+import {
+  Badge,
+  FlexWrapperBadge,
+  HomeStyle,
+  Input,
+} from "../style/home_styles";
 import AllCoins from "@modules/allCoins/content/allCoins";
 import NewCoins from "@modules/newCoins/content/newCoins";
 import FavoriteCoins from "@modules/favoriteCoins/content/favoriteCoins";
@@ -13,30 +18,32 @@ import GainerCoins from "@modules/gainerCoins/content/gainerCoins";
 import LoserCoins from "@modules/loserCoins/content/loserCoins";
 
 export default function Home() {
-  const [searchValue, setSearchValue] = useState("");
+  const [searchCoin, setSearchCoin] = useState("");
   const { TabPane } = Tabs;
   const [{ loading, data }, doFetch] = useFetch();
-
   const favoriteReducers = useSelector((state: any) => state.FavoriteReducer);
   const { favoriteList } = favoriteReducers;
 
-  const searchCoin = async (searchText: string) => {
+  const searchingCoin = async (searchValue: string) => {
     try {
       await doFetch({
-        url: `/search-suggestions?query=${searchText}`,
+        url: `/search-suggestions?query=${searchValue}`,
         method: "GET",
       });
     } catch (e) {}
   };
-  const handler = useCallback(debounce(searchCoin, 600), []);
-  const onSearch = (searchText: string) => {
-    setSearchValue(searchText);
-    handler(searchText);
+  const handler = useCallback(debounce(searchingCoin, 600), []);
+  const onSearch = (searchValue: string) => {
+    setSearchCoin(searchValue);
+    handler(searchValue);
   };
 
   return (
     <HomeStyle>
-      <input onChange={(e) => onSearch(e.target.value)} />(
+      <Input
+        placeholder="Search..."
+        onChange={(e) => onSearch(e.target.value)}
+      />
       <Tabs defaultActiveKey="1">
         <TabPane tab="All" key="1">
           <AllCoins />
@@ -62,7 +69,6 @@ export default function Home() {
           <LoserCoins />
         </TabPane>
       </Tabs>
-      )
     </HomeStyle>
   );
 }
