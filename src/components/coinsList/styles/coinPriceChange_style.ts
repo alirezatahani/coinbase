7e26ffff53f4611eb64adf45ca2a6c4ad57d589c
behzad <90694618+behzadiv@users.noreply.github.com>
2022-10-22 20:05:41 +0330

    fix color

--- src/components/coinsList/styles/coinPriceChange_style.ts
+++ src/components/coinsList/styles/coinPriceChange_style.ts
@@ -1,17 +1,17 @@
 import styled from "styled-components";
 
 export const CoinNegetiveChange = styled.p(({ theme }) => ({
-    fontSize: theme.typography.body1.fontSize,
-    color: theme.palette.success[600],
-    borderBottom: `1px solid ${theme.palette.success[400]}`,
-    borderRadius: 4,
-    flex: 1,
-  }));
-  
-  export const CoinPositiveChange = styled.p(({ theme }) => ({
-    fontSize: theme.typography.body1.fontSize,
-    color: theme.palette.danger[600],
-    borderBottom: `1px solid ${theme.palette.danger[400]}`,
-    borderRadius: 4,
-    flex: 1,
-  }));
+  fontSize: theme.typography.body1.fontSize,
+  color: theme.palette.danger[600],
+  borderBottom: `1px solid ${theme.palette.danger[400]}`,
+  borderRadius: 4,
+  flex: 1,
+}));
+
+export const CoinPositiveChange = styled.p(({ theme }) => ({
+  fontSize: theme.typography.body1.fontSize,
+  color: theme.palette.success[600],
+  borderBottom: `1px solid ${theme.palette.success[400]}`,
+  borderRadius: 4,
+  flex: 1,
+}));
