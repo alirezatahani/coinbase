ce0ff47bb5e4c68f21d08410eb4209867afb5db8
behzad <90694618+behzadiv@users.noreply.github.com>
2022-10-23 09:41:17 +0330

    use getCoinsData module in Home

--- src/pages/home/content/Home.tsx
+++ src/pages/home/content/Home.tsx
@@ -1,46 +1,73 @@
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { GetCoinsData ,FavoriteCoins} from "@modules/index";
 import { Tabs } from "antd";
 import { Badge, FlexWrapperBadge, HomeStyle } from "../style/home_styles";
-import { useSelector } from "react-redux";
-import React from "react";
-import AllCoins from "@modules/allCoins/content/allCoins";
-import NewCoins from "@modules/newCoins/content/newCoins";
-import FavoriteCoins from "@modules/favoriteCoins/content/favoriteCoins";
-import GainerCoins from "@modules/gainerCoins/content/gainerCoins";
-import LoserCoins from "@modules/loserCoins/content/loserCoins";
 import "antd/dist/antd.css";
 
 export default function Home() {
   const { TabPane } = Tabs;
-
+  const [limit, setLimit] = useState(10);
   const favoriteReducers = useSelector((state: any) => state.FavoriteReducer);
   const { favoriteList } = favoriteReducers;
 
+  const tabs = [
+    {
+      name: "All",
+      key: "1",
+      queries: {},
+    },
+    {
+      name: "Favorite Coins",
+      key: "2",
+    },
+    {
+      name: "Gainers",
+      key: "3",
+      queries: { orderBy: "change", orderDirection: "desc", scopeLimit: 200 },
+    },
+    ,
+    {
+      name: "Losers",
+      key: "4",
+      queries: { orderBy: "change", orderDirection: "asc", scopeLimit: 200 },
+    },
+    {
+      name: "New",
+      key: "5",
+      queries: { orderBy: "listedAt", orderDirection: "desc" },
+    },
+  ];
+
   return (
     <HomeStyle>
-      <Tabs defaultActiveKey="1">
-        <TabPane tab="All" key="1">
-          <AllCoins />
-        </TabPane>
-        <TabPane
-          tab={
-            <FlexWrapperBadge>
-              <span>Favorite Coins</span>
-              <Badge>{favoriteList.length}</Badge>
-            </FlexWrapperBadge>
+      <Tabs>
+        {tabs.map((tab) => {
+          const { key, name, queries } = tab;
+          switch (name) {
+            case "Favorite Coins":
+              return (
+                <TabPane
+                  tab={
+                    <FlexWrapperBadge>
+                      <span>Favorite Coins</span>
+                      <Badge>{favoriteList.length}</Badge>
+                    </FlexWrapperBadge>
+                  }
+                  key={key}
+                >
+                  <FavoriteCoins />
+                </TabPane>
+              );
+
+            default:
+              return (
+                <TabPane tab={name} key={key}>
+                  <GetCoinsData queries={{ ...queries, limit }} />
+                </TabPane>
+              );
           }
-          key="2"
-        >
-          <FavoriteCoins />
-        </TabPane>
-        <TabPane tab="New" key={3}>
-          <NewCoins />
-        </TabPane>
-        <TabPane tab="Gainers" key={4}>
-          <GainerCoins />
-        </TabPane>
-        <TabPane tab="Losers" key={5}>
-          <LoserCoins />
-        </TabPane>
+        })}
       </Tabs>
     </HomeStyle>
   );
