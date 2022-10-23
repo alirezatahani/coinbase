eae9e363e0d4e97e590dc7dc1432d879e371e6d0
behzad <90694618+behzadiv@users.noreply.github.com>
2022-10-23 10:30:18 +0330

    get color from theme

--- src/components/coinsList/styles/coinListContainer_style.ts
+++ src/components/coinsList/styles/coinListContainer_style.ts
@@ -3,11 +3,11 @@ import styled from "styled-components";
 export const CoinListContainer = styled.div(() => ({
   marginTop: "1rem",
 }));
-export const CoinContent = styled.div(({}) => ({
+export const CoinContent = styled.div(({theme}) => ({
   display: "flex",
   gap: "2rem",
   marginBottom: "1rem",
-  color: "white",
+  color: theme.palette.common.white,
 }));
 
 export const CoinDesc = styled.div(({ theme }) => ({
