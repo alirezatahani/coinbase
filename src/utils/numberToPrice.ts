c8db39827ac65a9c4cb7f4d71d79adeae57c2fc0
behzad <90694618+behzadiv@users.noreply.github.com>
2022-11-15 10:36:22 +0330

    fix numberToPrice

--- src/utils/numberToPrice.ts
+++ src/utils/numberToPrice.ts
@@ -1,5 +1,6 @@
 export const numberToPrice = (price: number) => {
   return price.toLocaleString(undefined, {
-    minimumFractionDigits: price <= 0.0001 ? 8 : 3,
+    minimumFractionDigits:
+      price <= 0.0001 ? 8 : 0.0001 < price && price < 1 ? 3 : 0,
   });
 };
