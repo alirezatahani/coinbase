643d4eb68959af6d113b39c93ffe43405e8ad357
behzad <90694618+behzadiv@users.noreply.github.com>
2022-12-18 13:29:01 +0330

    seprate options as utils

--- src/components/homeTabs/content/HomeTabs.tsx
+++ src/components/homeTabs/content/HomeTabs.tsx
@@ -4,6 +4,7 @@ import { FavoriteCoins } from "@modules/index";
 import { GetCoinsData } from "@modules/index";
 import FavLengthBadge from "./FavLengthBadge";
 import { tabs } from "../utils/tabs";
+import { options } from "../utils/selectOptions";
 
 const HomeTabs = () => {
   const [limit, setLimit] = useState(10);
@@ -15,53 +16,12 @@ const HomeTabs = () => {
   };
 
   return (
-    <div>
+    <>
       <Select
         defaultValue="24h"
         onChange={handleChange}
         style={{ width: 120 }}
-        options={[
-          {
-            value: "1h",
-            label: "1 hour",
-          },
-          {
-            value: "3h",
-            label: "3 hour",
-          },
-          {
-            value: "12h",
-            label: "12 hours",
-          },
-          {
-            value: "24h",
-            label: "24 hours",
-          },
-          {
-            value: "7d",
-            label: "7 days",
-          },
-          {
-            value: "30d",
-            label: "30 days",
-          },
-          {
-            value: "3m",
-            label: "3 months",
-          },
-          {
-            value: "1y",
-            label: "1 year",
-          },
-          {
-            value: "3y",
-            label: "3 years",
-          },
-          {
-            value: "5y",
-            label: "5 years",
-          },
-        ]}
+        options={options}
       />
       <Tabs>
         {tabs.map((tab) => {
@@ -83,7 +43,7 @@ const HomeTabs = () => {
           }
         })}
       </Tabs>
-    </div>
+    </>
   );
 };
 
---
+++ src/components/homeTabs/utils/selectOptions.ts
@@ -0,0 +1,42 @@
+export const options = [
+  {
+    value: "1h",
+    label: "1 hour",
+  },
+  {
+    value: "3h",
+    label: "3 hour",
+  },
+  {
+    value: "12h",
+    label: "12 hours",
+  },
+  {
+    value: "24h",
+    label: "24 hours",
+  },
+  {
+    value: "7d",
+    label: "7 days",
+  },
+  {
+    value: "30d",
+    label: "30 days",
+  },
+  {
+    value: "3m",
+    label: "3 months",
+  },
+  {
+    value: "1y",
+    label: "1 year",
+  },
+  {
+    value: "3y",
+    label: "3 years",
+  },
+  {
+    value: "5y",
+    label: "5 years",
+  },
+];
