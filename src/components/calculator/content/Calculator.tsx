f00bd8673bb6e149ad0ff34a3694fcbdcbdc4fc6
behzad <90694618+behzadiv@users.noreply.github.com>
2022-12-25 11:51:03 +0330

    get options by fetch data

--- src/components/calculator/content/Calculator.tsx
+++ src/components/calculator/content/Calculator.tsx
@@ -1,13 +1,29 @@
 import { Select } from "antd";
 import useFetch from "../../../hooks/useFetch";
 import React, { useEffect, useState } from "react";
-import {
-  Input,
-  InputRowContainer,
-} from "../style/calculator_style";
+import { Input, InputRowContainer } from "../style/calculator_style";
 
 const Calculator = () => {
-  const [options, setOptions]: any = useState([]);
+    const [options, setOptions]: any = useState([]);
+    const [{ data, loading }, fetchCoinData] = useFetch();
+  
+  useEffect(() => {
+    fetchCoinData({ url: `/coins`, method: "get" });
+  }, []);
+  useEffect(() => {
+    createOptions();
+  }, [data]);
+  const createOptions = () => {
+    let options: any = [];
+    !loading &&
+      data?.data.coins.map((item: any) => {
+        return options.push({
+          value: item.price,
+          label: item.symbol + "-" + item.name,
+        });
+      });
+    return setOptions(options);
+  };
   
   return (
     <div>
@@ -17,8 +33,8 @@ const Calculator = () => {
         cryptocurrencies and fiat.
       </p>
       <InputRowContainer>
-        <Input type="number" />
-        <Select options={options} />
+        <Input type="number" name="firstValue"  />
+        <Select options={options}  />
       </InputRowContainer>
     </div>
   );
