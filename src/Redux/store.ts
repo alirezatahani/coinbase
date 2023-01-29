0cdd0adda0596aaa61a45546e8675efafdf12b19
behzad <90694618+behzadiv@users.noreply.github.com>
2023-01-29 12:16:29 +0330

    add timePeriod to redux

--- src/Redux/store.ts
+++ src/Redux/store.ts
@@ -4,12 +4,14 @@ import { stackReducer } from "./stack/stackReducer";
 import { loadState } from "../localStorage";
 import { themeReducer } from "./userTheme/userThemeReducer";
 import { referenceCurrencyReducer } from "./referenceCurrency/referenceCurrencyReducer";
+import { timePeriodReducer } from "./timePeriod/timePeriodReducer";
 
 const reducer = combineReducers({
   FavoriteReducer: favoriteReducer,
   stack: stackReducer,
   theme: themeReducer,
   referenceCurrency: referenceCurrencyReducer,
+  timePeriod: timePeriodReducer,
 });
 
 const preloadedState = loadState();
@@ -20,5 +22,5 @@ const store = configureStore({ reducer, preloadedState });
 
 export default store;
 
-export type RootState = ReturnType<typeof store.getState>
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
