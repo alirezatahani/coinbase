b7b241d670d0750e7a12fea3e56d611f0d2ecea8
behzad <90694618+behzadiv@users.noreply.github.com>
2023-02-13 14:30:21 +0330

    create initial footer component

---
+++ src/components/footer/content/Footer.tsx
@@ -0,0 +1,75 @@
+import React, { useEffect } from "react";
+import { CoinInterface } from "types";
+import useFetch from "../../../hooks/useFetch";
+import { useAppSelector } from "hooks/hooks";
+import { numberToPrice } from "@utils/numberToPrice";
+import { convertToQuery } from "@utils/queries";
+import { Spin } from "antd";
+import {
+  FavCoinChange,
+  FavCoinDesc,
+  FavCoinTitle,
+  FooterContainer,
+  EmptyText,
+  FavCoinCard,
+} from "../style/footer_style";
+
+const Footer = () => {
+  const { favoriteList } = useAppSelector((state) => state.FavoriteReducer);
+  const { sign, value } = useAppSelector((state) => state.referenceCurrency);
+  const timePeriod = useAppSelector((state) => state.timePeriod.timePeriod);
+  const [{ loading, data }, fetchCoinsData] = useFetch();
+
+  const uuidsString = favoriteList
+    .map((item: string, index: number) => {
+      let query = "";
+      if (index === 0) query = item;
+      else query = `uuids[]=${item}`;
+      return query;
+    })
+    .join("&");
+
+  const queries = { timePeriod, value, uuids: uuidsString };
+
+  const makingUrl = () => {
+    return convertToQuery(queries);
+  };
+
+  useEffect(() => {
+    const url = makingUrl();
+    fetchCoinsData({ url: `/coins?${url}`, method: "get" });
+  }, [favoriteList]);
+
+  if (loading)
+    return (
+      <FooterContainer>
+        <Spin />
+      </FooterContainer>
+    );
+  return (
+    <FooterContainer>
+      {favoriteList.length ? (
+        data &&
+        data.data.coins.map((coin: CoinInterface) => (
+          <FavCoinCard>
+            <img src={coin.iconUrl} style={{ width: "40px" }} />
+            <FavCoinDesc>
+              <FavCoinTitle>
+                <span>{coin.symbol}</span>
+                <FavCoinChange change={Number(coin.change)}>
+                  {Number(coin.change) > 0
+                    ? `+ ${coin.change}`
+                    : `${coin.change}`}
+                </FavCoinChange>
+              </FavCoinTitle>
+              <span>{numberToPrice(Number(coin.price), sign)}</span>
+            </FavCoinDesc>
+          </FavCoinCard>
+        ))
+      ) : (
+        <EmptyText>favorite coins list is empty ... </EmptyText>
+      )}
+    </FooterContainer>
+  );
+};
+export default Footer;
---
+++ src/components/footer/style/footer_style.ts
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+
+type FavCoinProps = {
+  change: number;
+};
+
+export const FooterContainer = styled.div(({ theme }) => ({
+  width: "550px",
+  minHeight: "130px",
+  position: "fixed",
+  bottom: 0,
+  zIndex: 999,
+  backgroundColor: "#101820FF",
+  color: "#FEE715FF",
+  padding: "1rem 2rem",
+  display: "flex",
+  alignItems: "center",
+  gap: "1.5rem",
+  overflowX: "scroll",
+  "&::-webkit-scrollbar": {
+    height: "0.4rem",
+  },
+  "&::-webkit-scrollbar-thumb": {
+    backgroundColor: theme.palette.background.color,
+    outline: `.5px solid ${theme.palette.background.color}`,
+    borderRadius: "5px",
+  },
+}));
+export const FavCoinCard = styled.div(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  padding:"1rem",
+  border:"1px solid #333"
+}));
+export const FavCoinDesc = styled.div(({ theme }) => ({
+  display: "flex",
+  flexDirection: "column",
+  rowGap: ".2rem",
+}));
+export const FavCoinTitle = styled.div(({ theme }) => ({
+  position: "relative",
+}));
+export const FavCoinChange = styled.span<FavCoinProps>(({ theme, change }) => ({
+  position: "absolute",
+  top: -13,
+  right: -20,
+  color: change >= 0 ? theme.palette.success[600] : theme.palette.danger[600],
+}));
+export const EmptyText = styled.p(({ theme }) => ({
+  width: "100%",
+  textAlign: "center",
+}));
